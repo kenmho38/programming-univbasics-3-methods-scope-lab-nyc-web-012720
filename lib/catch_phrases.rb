ecd9f54d995a1f5a8039a0f81a3b90e12cdb242b
Ken Ho <kenmho38@hotmail.com>
2020-01-08 14:59:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,11 @@
-phrase = "It's-a me, Mario!"
 
-def mario
+
+def mario(phrase)
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
+
+mario("It's-a me, Mario!")
 
 def toadstool
   puts status
