e23001d6ace29e7a0b4a1b321ea398bc8f3056c1
Ken Ho <kenmho38@hotmail.com>
2020-01-08 15:01:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
 
 
-def mario("It's-a me, Mario!")
+def mario(phrase)
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  phrase = "It's-a me, Mario!"
 end
 
 mario("It's-a me, Mario!")
