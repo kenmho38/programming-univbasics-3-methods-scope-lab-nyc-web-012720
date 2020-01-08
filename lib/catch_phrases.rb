8488671b27e9f7f0cc82a83b77568002b06a59ae
Ken Ho <kenmho38@hotmail.com>
2020-01-08 15:07:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,11 +2,22 @@
 
 def mario(phrase)
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "#{phrase}""
   phrase = "It's-a me, Mario!"
+
 end
 
 mario
+
+
+
+
+def visit_wizarding_world(name)
+  simple_name = "Hogwart's"
+  puts "#{name}, a #{$species}, visits #{simple_name}"
+end
+
+
 
 def toadstool
   puts status
